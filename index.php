54d641660c8e07e3268abea931ffac3386806da8
Peter Manchev <p.manchev@emerchantpay.com>
2015-01-08 17:05:15 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+/*
+Plugin Name: WooCommerce eMerchantPay Payment Gateway Client
+Description: Extends WooCommerce's Checkout with eMerchantPay/Genesis Gateway
+Version: 1.0.0
+*/
+
+if ( !function_exists('woocommerce_ecomprocessing_init') ):
+	function woocommerce_ecomprocessing_init()
+	{
+	    if ( !class_exists( 'WC_Payment_Gateway' ) ) return;
+
+		// Load text Domain
+	    load_plugin_textdomain('woocommerce_ecomprocessing', false, 'languages');
+
+		// Get Genesis class
+		include dirname(__FILE__) . '/includes/WC_EComProcessing_Checkout.php';
+
+		/**
+		 * Add the EComProcessing Gateway to WooCommerce's
+		 * list of installed gateways
+		 *
+		 * @param $methods Array of existing Payment Gateways
+		 *
+		 * @return array $methods Appended Payment Gateway
+		 */
+		if ( !function_exists('woocommerce_add_ecomprocessing_gateway') ):
+		    function woocommerce_add_ecomprocessing_gateway($methods) {
+			    array_push($methods, 'WC_EComProcessing_Checkout');
+		        return $methods;
+		    }
+		endif;
+
+	    add_filter('woocommerce_payment_gateways', 'woocommerce_add_ecomprocessing_gateway' );
+	}
+endif;
+add_action('plugins_loaded', 'woocommerce_ecomprocessing_init', 0);
